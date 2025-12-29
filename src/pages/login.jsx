feadc08597eae49c2dd95ee8df5679b3ad1e9d9f
ifeomare <ifeoma.ejikeme@outlook.com>
2025-12-29 18:39:45 -0500

    SCH-15 Designed Log In Page

---
+++ src/pages/login.jsx
@@ -0,0 +1,34 @@
+import "../styles/login.css";
+import Navbar from "../components/navbar.jsx";
+function Login() {
+  return (
+    <>
+      <Navbar />
+      <div className="page-format">
+        <aside className="aside-border"></aside>
+        <main className="login-container">
+          <form className="login-form">
+            <h1 className="form-header">
+              Log In To Your Account
+              <br />
+              Here
+            </h1>
+            <div className="input-styles">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="input-styles">
+              <label htmlFor="password">Password</label>
+              <input type="text" name="password" id="password" />
+            </div>
+            <div className="submit-button">
+              <input type="submit" value="Log In" id="login"/>
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default Login;
